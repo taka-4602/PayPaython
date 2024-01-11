1e8078d86ddf6fbdad481383bbb19dd14d6da28b
taka-4602 <116678390+taka-4602@users.noreply.github.com>
2024-01-11 20:11:08 +0900

    Add files via upload

--- PayPaython/__init__.py
+++ PayPaython/__init__.py
@@ -1,3 +1,3 @@
 from .main import PayPay
-__version__      = '0.0.1'
+__version__      = '1.0.0'
 __url__          = 'https://github.com/taka-4602/PayPaython'
--- PayPaython/main.py
+++ PayPaython/main.py
@@ -1,5 +1,4 @@
 import requests
-import json
 import datetime
 import uuid
 
