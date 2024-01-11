8194c7e8c795de14f1b6b6b6249b2a881d0d0fa5
taka-4602 <116678390+taka-4602@users.noreply.github.com>
2024-01-11 17:32:11 +0900

    Add files via upload

---
+++ lets go.py
@@ -0,0 +1,27 @@
+import PayPaython
+
+#ログイン
+paypay=PayPaython.PayPay()
+print(paypay)
+#送金リンク受け取り
+print(paypay.receive("osuvUuLmQH8WA4kW"))#ぺいぺい送金リンクの https://pay.paypay.ne.jp/osuvUuLmQH8WA4kW <-ここね
+#送金リンクを辞退
+print(paypay.reject("osuvUuLmQH8WA4kW"))
+#送金リンクを作成
+print(paypay.create_link(kingaku=1,password=1111))
+#残高確認
+print(paypay.balance())
+#ユーザー情報
+print(paypay.user_info())
+#ユーザーの表示情報
+print(paypay.display_info())
+#ユーザーの支払い方法
+print(paypay.payment_method())
+#取引履歴
+print(paypay.history())
+#指定したexternalidのユーザーに直接送金
+print(paypay.send_money(kingaku=1,externalid="048f4fef00bdbc00"))
+#送金してもらうためのURLを作成する(PayPayアプリのQRコードとおなじ)
+print(paypay.create_p2pcode())
+#支払いのワンタイムコードを作成する
+print(paypay.create_payment_otcfh())
